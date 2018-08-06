ccdf2b80661f00fbf6494ed499aca0e15d797022
Christopher Crouch <chrisdc@gmail.com>
2018-08-06 15:57:47 +0100

    Start adding file type tests

--- test/main.js
+++ test/main.js
@@ -1,3 +1,5 @@
+/* eslint-env node, mocha */
+
 const fs = require('fs');
 var chai = require('chai');
 var expect = chai.expect;
@@ -6,19 +8,53 @@ var fixtures;
 
 describe('identify-stream', function() {
   before(function(done) {
-		fs.readdir('./test/fixtures', function(err, files) {
+    fs.readdir('./test/fixtures', function(err, files) {
       fixtures = files;
     });
 
-		done();
-  });
-
-  it('Should load.', function(done) {
-    expect(typeof IdentifyStream).to.equal('object');
     done();
   });
 
-  it('Should identify custom files.');
+  it('Should load.', function() {
+    expect(typeof IdentifyStream).to.equal('function');
+    //done();
+  });
 
-  it('Should reject unknown file type.');
+  describe('Identify Types', function() {
+    var inputStream,
+        outputStream,
+        indentifyStream;
+
+    beforeEach() {
+      var indentifyStream = new IdentifyStream();
+      var inputStream = fs.createReadStream('./test/fixtures/fixture.jpg');
+      var outputStream = fs.createWriteStream('output.flac');
+    }
+
+    it('Should identify a jpg.', function(done) {
+      var indentifyStream = new IdentifyStream();
+      var inputStream = fs.createReadStream('./test/fixtures/fixture.jpg');
+      var outputStream = fs.createWriteStream('output.flac');
+
+      indentifyStream.on('identified', (type) => {
+        expect(type).to.equal('jpeg');
+        done();
+      });
+
+      inputStream.pipe(indentifyStream).pipe(outputStream);
+    });
+
+    it('Should identify a gif.', function(done) {
+      var indentifyStream = new IdentifyStream();
+      var inputStream = fs.createReadStream('./test/fixtures/fixture.gif');
+      var outputStream = fs.createWriteStream('output.flac');
+
+      indentifyStream.on('identified', (type) => {
+        expect(type).to.equal('gif');
+        done();
+      });
+
+      inputStream.pipe(indentifyStream).pipe(outputStream);
+    });
+  });
 });
