1b976fc19b32061ed45b9b07353f899cde80adea
Christopher Crouch <chrisdc@gmail.com>
2018-08-13 15:58:43 +0100

    Move test-file util into new file

---
+++ test/utils/test-file.js
@@ -0,0 +1,25 @@
+/* eslint-env node, mocha */
+const IdentifyStream = require('../../index.js');
+const fs = require('fs');
+const { expect } = require('chai');
+const { WritableStreamBuffer } = require('stream-buffers');
+
+function testFile(path, expected, options, cb) {
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+
+  const inputStream = fs.createReadStream(path);
+  const identifyStream = new IdentifyStream(options);
+  const writableStreamBuffer = new WritableStreamBuffer();
+
+  identifyStream.on('identified', (type) => {
+    expect(type).to.equal(expected);
+    cb();
+  });
+
+  inputStream.pipe(identifyStream).pipe(writableStreamBuffer);
+}
+
+module.exports = testFile;
